b0810e067c3804241a542e20576f858e44cc483a
anatoliy.kruglov <002_1990@mail.ru>
2021-01-26 19:40:30 +0300

    new file

---
+++ parse.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def parse_asos(url):
+    nextPage = True
+    new_news = []
+    news = []
+
+    while nextPage:
+        page = getPage(url)
+        soup = BeautifulSoup(page.text, "lxml")
+
+        news = soup.findAll('a', class_='_3TqU78D')
+        for i in range(len(news)):
+            if news[i].find('span', class_='_16nzq18') is not None:
+                try:
+                    price = news[i].find('span', {'class': '_16nzq18'}).text
+                    price_sale = news[i].find('span', {'class': '_22sbBtS'}).text
+
+                    price = float(price.replace('руб.', '').replace(',', '.').replace(' ', '').replace('От', ''))
+                    price_sale = float(
+                        price_sale.replace('руб.', '').replace(',', '.').replace(' ', '').replace('От', ''))
+
+                    dictionary = {
+                        'name': news[i].text,
+                        'url': news[i].get('href'),
+                        'price': price,
+                        'price_sale': price_sale,
+                        'sale': int((price_sale / price - 1) * 100)
+                    }
+                    new_news.append(dictionary)
+                except:
+                    continue
+        if soup.find(rel="next") is not None:
+            url = str(soup.find(rel="next").get('href'))
+        else:
+            nextPage = False
+
+    return new_news
+
+
+def getPage(url):
+    session = requests.Session()
+    session.headers.update({
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    })
+    return session.get(url, verify=False)
