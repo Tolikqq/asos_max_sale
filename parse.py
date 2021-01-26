9a60e0f3998d03e667edbbaef2c5aeef83790b5b
anatoliy.kruglov <002_1990@mail.ru>
2021-01-26 20:22:34 +0300

    verify

--- parse.py
+++ parse.py
@@ -44,4 +44,4 @@ def getPage(url):
     session.headers.update({
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
     })
-    return session.get(url, verify=False)
+    return session.get(url, verify=True)
